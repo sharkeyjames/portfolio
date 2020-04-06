43f4f3aa7c1bb7b8a2f27d50361efe8afc2b43ff
James Sharkey <james.sharkey@wearewiser.com>
2020-04-06 12:47:24 +0100

    Expose Card and List components

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { ListComponent } from '../components/list/list.component';
+import { CardComponent } from '../components/card/card.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    ListComponent,
+    CardComponent,
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
