757f2a9e06d9731cd9b4b2c1fc08cedc1d28d752
James Sharkey <james.sharkey@wearewiser.com>
2020-04-07 13:39:33 +0100

    Remove redundant import

--- src/app/services/project/project.service.ts
+++ src/app/services/project/project.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { Project } from '../../models/project'
 import { first, filter, timeout } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
